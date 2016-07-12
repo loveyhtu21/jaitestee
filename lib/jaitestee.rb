be89660ad72882c54d90a50e418f6a74d94da8c0
Jai <loveyhtu21@gmail.com>
2016-07-12 14:18:48 +0530

    initial publiced

---
+++ lib/jaitestee.rb
@@ -0,0 +1,8 @@
+require "jaitestee/version"
+
+module Jaitestee
+  # Your code goes here...
+  def self.author
+  	puts "My name is Jai Rajput. Thanks for using this gem in your application."
+  end
+end
---
+++ lib/jaitestee/version.rb
@@ -0,0 +1,3 @@
+module Jaitestee
+  VERSION = "0.1.0"
+end
