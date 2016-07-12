f783a5d2defecc39ea1ddc54efeaddd9f96d7527
Jai <loveyhtu21@gmail.com>
2016-07-12 15:03:06 +0530

    make it better

--- lib/jaitestee.rb
+++ lib/jaitestee.rb
@@ -1,8 +1,10 @@
-require "jaitestee/version"
+require "jaitestee"
 
 module Jaitestee
   # Your code goes here...
-  def self.author
-  	puts "My name is Jai Rajput. Thanks for using this gem in your application."
-  end
+  class Author
+	  def self.intro
+	  	puts "My name is Jai Rajput. Thanks for using this gem in your application."
+	  end
+	end
 end
